ed8f6fc4e55cad3f6f1a1c9166dce5090e02a8f3
Antoniolifer <ant.serdiuk@gmail.com>
2025-03-17 18:04:32 +0800

    css style conflicts

--- src/components/FilterCheckBox.jsx
+++ src/components/FilterCheckBox.jsx
@@ -4,8 +4,8 @@ function FilterCheckBox({category, checked, toggleBox}){
     toggleBox(category);
   }
   return(
-    <div onClick={handleToggle} className="hover:font-bold hover:cursor-pointer p-1 mx-1 hover:text-violet-600">
-    <input type="checkbox" name={category} id="" onChange={()=> ''} className="mr-1 mt-1 accent-violet-600 hover:cursor-pointer" checked={checked}/>
+    <div onClick={handleToggle} className="w-full hover:font-bold hover:cursor-pointer my-1 mx-1 hover:text-violet-600">
+    <input type="checkbox" name={category} id="" readOnly className="mr-1 mt-1 accent-violet-600 hover:cursor-pointer" checked={checked}/>
     <label htmlFor={category} className="hover:cursor-pointer">{category}</label><br/>
     </div>
   )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = ({ isOpen, onClose, children }) => {
                         bg-black/75" onClick={handleClick}>
             <div className="bg-gray-200 rounded-xl
                             shadow-lg p-6 m-2 max-w-5xl top-5 md:top-15
-                            w-4/5 fixed overflow-auto overscroll-contain max-h-[700px]">
+                            w-4/5 absolute overflow-auto overscroll-contain max-h-[90vh]">
                 <button
                     className="absolute top-2 right-2 text-2xl
                                text-gray-200 hover:text-gray-700
